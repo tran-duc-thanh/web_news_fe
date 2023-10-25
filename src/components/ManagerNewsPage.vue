25abfaf6b2d7455787b9054408bcd637c9c67d39
tran-duc-thanh <thanh.tranduc@savis-digital.com>
2023-10-25 18:08:16 +0700

    commit code 25/10/23

--- src/components/ManagerNewsPage.vue
+++ src/components/ManagerNewsPage.vue
@@ -16,7 +16,7 @@ export default {
     data() {
         return {
             editor: ClassicEditor,
-            editorData: '<p>Content of the editor.</p>',
+            editorData: '',
             editorConfig: {
                 // The configuration of the editor.
             }
