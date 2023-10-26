bcc6d4ae94efc02c7cf6538cbec1cdeded7f8a85
Tran Duc Thanh <tranducthanh04101996@gmail.com>
2023-10-26 23:40:34 +0700

    commit code

--- src/components/ManagerNewsPage.vue
+++ src/components/ManagerNewsPage.vue
@@ -10,10 +10,28 @@
         </div>
     </div>
     <!-- Main Breadcrumb End -->
+
     <div class="main-content--section pbottom--30">
         <div class="container">
             <div class="main--content">
-                <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+                <div style="margin-top: 20px">Tiêu đề</div>
+                <input style="width: 100%; height: 50px; margin-top: 5px;" v-model="message" placeholder="News title." />
+                <div style="margin-top: 20px">Thể loại: {{ selected }}</div>
+                <select style="width: 100%; margin-top: 5px;" v-model="selected" multiple>
+                    <option v-for="option in options" :key="option.value" :value="option.value">
+                        {{ option.text }}
+                    </option>
+                </select>
+                <div style="margin-top: 20px">Thẻ: {{ selected }}</div>
+                <select style="width: 100%; margin-top: 5px;" v-model="selected" multiple>
+                    <option v-for="option in options" :key="option.value" :value="option.value">
+                        {{ option.text }}
+                    </option>
+                </select>
+                <div style="margin-top: 20px">Nội dung</div>
+                <div style="margin-top: 5px">
+                    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+                </div>
             </div>
         </div>
     </div>
@@ -27,10 +45,19 @@ export default {
     data() {
         return {
             editor: ClassicEditor,
-            editorData: '',
+            editorData: 'News content.',
             editorConfig: {
                 // The configuration of the editor.
-            }
+            },
+            selected: [],
+            options: [
+                { text: 'Văn hóa', value: 'VAN_HOA' },
+                { text: 'Chính trị', value: 'CHINH_TRI' },
+                { text: 'Khoa học', value: 'KHOA_HOC' },
+                { text: 'Thể thao', value: 'THE_THAO' },
+                { text: 'Thế giới', value: 'THE_GIOI' },
+                { text: 'Xã hội', value: 'XA_HOI' }
+            ]
         };
     }
 }
