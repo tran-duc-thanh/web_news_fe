4ef3d2e199df474bd87f3f1beefe1e104bc821de
tran-duc-thanh <thanh.tranduc@savis-digital.com>
2023-10-26 18:25:11 +0700

    commit code 26/10/23

--- src/components/ManagerNewsPage.vue
+++ src/components/ManagerNewsPage.vue
@@ -1,4 +1,15 @@
 <template>
+    <!-- Main Breadcrumb Start -->
+    <div class="main--breadcrumb">
+        <div class="container">
+            <ul class="breadcrumb">
+                <li><a href="home-1.html" class="btn-link"><i class="fa fm fa-home"></i>Home</a></li>
+                <li class="active"><span>World News</span></li>
+                <li class="active"><span>Manager news</span></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Main Breadcrumb End -->
     <div class="main-content--section pbottom--30">
         <div class="container">
             <div class="main--content">
